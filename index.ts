2c2880b112edec13582ff0e0a763614eec24cf55
Matheus Viana <matheus_o_viana@hotmail.com>
2020-01-27 21:58:15 -0300

    chore fixed bugs

--- index.ts
+++ index.ts
@@ -1,13 +1,12 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
+import * as express from 'express';
+import * as mongoose from 'mongoose';
+import * as bodyParser from 'body-parser';
 import routes from './src/routes/crmRoutes';
 
 const app = express();
 const PORT = 3000;
 
 // mongoose connection
-mongoose.Promise = global.Promise;
 mongoose.connect(
   'mongodb://movian:datatest@linkedinproject-shard-00-00-8w1w5.mongodb.net:27017,linkedinproject-shard-00-01-8w1w5.mongodb.net:27017,linkedinproject-shard-00-02-8w1w5.mongodb.net:27017/test?ssl=true&replicaSet=linkedinProject-shard-0&authSource=admin&retryWrites=true&w=majority',
   {
