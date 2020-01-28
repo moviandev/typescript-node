919e6562a94d49cbecf013772afcc3596595cb84
Matheus Viana <matheus_o_viana@hotmail.com>
2020-01-27 22:50:04 -0300

    chore: added namespace example

--- src/controllers/createMessage.ts
+++ src/controllers/createMessage.ts
@@ -5,3 +5,13 @@ export class Messenger {
     return `Node and express is running on port ${this.port}`;
   }
 }
+
+// namespace Messengespace {
+//   export class Messenger {
+//     constructor(public port: number) {}
+
+//     messagePrint() {
+//       return `Node and express is running on port ${this.port}`;
+//     }
+//   }
+// }
