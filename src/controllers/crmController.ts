cbd9dc825a5efa855e2777d89bc3cd26e53b7100
Matheus Viana <matheus_o_viana@hotmail.com>
2020-01-27 21:48:42 -0300

    chore: changed js to ts and added @types

--- src/controllers/crmController.ts
+++ src/controllers/crmController.ts
@@ -42,7 +42,7 @@ export const updateContact = (req, res) => {
 }
 
 export const deleteContact = (req, res) => {
-    Contact.remove({ _id: req.params.contactId }, (err, contact) => {
+    Contact.remove({ _id: req.params.contactId }, (err: any) => {
         if (err) {
             res.send(err);
         }
